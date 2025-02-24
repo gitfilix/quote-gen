27c4bb9d60cfd2b50b287ee583503cf427ad2028
gitfilix <runlevel07@gmail.com>
2025-02-24 21:46:25 +0100

    Q: added fonts & stylings effects and basic markup

--- quote-app/src/Components/QuotesApp.jsx
+++ quote-app/src/Components/QuotesApp.jsx
@@ -1,6 +1,31 @@
 function QuotesApp() {
   return (
-    <div>QuotesApp</div>
+    <div className='container'>
+        <div className='quote-app'>
+            <h1 className='app-header'>Quote.</h1>
+            <i className='bx bxs-heart fav-icon'></i>
+            <div className='quote'>
+              <i className='bx bxs-quote-alt-left left-quote'></i>
+              <p className='quote-text'>“The only way to do great work is to love what you do.”</p>
+              <p className='quote-author'>- Steve Jobs</p>
+              <i className='bx bxs-quote-alt-right right-quote'></i>
+            </div>
+            <div className='circles'>
+              <div className='circle-1'></div>
+              <div className='circle-2'></div>
+              <div className='circle-3'></div>
+              <div className='circle-4'></div>
+            </div>
+            <div className='buttons'>
+              <button className='btn btn-new'>
+                New Quote
+              </button>
+              <button className='btn btn-fav'>
+                Add to Favorite
+              </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
