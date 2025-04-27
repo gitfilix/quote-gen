8820fecad629db569075c848ffe00877e3ab4296
gitfilix <runlevel07@gmail.com>
2025-04-27 14:39:20 +0200

    Q: changed the vite config

--- README.md
+++ README.md
@@ -21,3 +21,14 @@ npm run deploy
 ## using the ninja quote generator
 https://api.api-ninjas.com
 
+
+
+## deployment on render.com
+
+add to package.json the optional rollup for no real reason but it works during the build:
+
+```
+  "optionalDependencies": {
+    "@rollup/rollup-linux-x64-gnu": "4.6.1"
+  }
+```
--- quote-app/vite.config.js
+++ quote-app/vite.config.js
@@ -3,7 +3,21 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const isProduction = process.env.NODE_ENV === 'production'
 // https://vite.dev/config/
+// for render.com we need to set the port to 10000
 export default defineConfig({
   plugins: [react()],
+  server: {
+      host: true,
+      strictPort: true,
+      port: isProduction ? 10000: 5173,
+      allowedHosts: [
+        'flx-chatbot.local',
+        'flx-chatbot.local:10000',
+        'flx-chatbot.local:5173',
+        'localhost',
+        'quote-generator-kd9f.onrender.com'
+      ],
+  }
 })
